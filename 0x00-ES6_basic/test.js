5caf3729eca2fce3900d547688502b12001ef446
youssef3332-a11y <youssef17102003@gmail.com>
2024-10-02 20:53:02 +0100

    Refactor block-scoped function and add test file

--- 0x00-ES6_basic/test.js
+++ 0x00-ES6_basic/test.js
@@ -1,2 +1,4 @@
-import {taskFirst, taskNext} from './0-constants.js';
-console.log(`${taskFirst()} ${taskNext()}`);
+import taskBlock from './1-block-scoped.js';
+
+console.log(taskBlock(true));
+console.log(taskBlock(false));
