185271a428c30fb3b0d2b9fd38d1c9d0f6cf654e
youssef3332-a11y <youssef17102003@gmail.com>
2024-10-03 00:08:19 +0100

    update file

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,10 @@ export default function taskBlock(trueOrFalse) {
     const task2 = true;
   
     if (trueOrFalse) {
-      let task = true;
-      let task2 = false;
+      // eslint-disable-next-line no-unused-vars, no-shadow
+      const task = true;
+      // eslint-disable-next-line no-unused-vars, no-shadow
+      const task2 = false;
     }
   
     return [task, task2];
