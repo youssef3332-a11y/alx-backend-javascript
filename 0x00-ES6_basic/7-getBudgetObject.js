58fb9b88313ffb4c518bc2911f2439214b3cbf49
youssef3332-a11y <youssef17102003@gmail.com>
2024-10-03 01:01:00 +0100

    Refactor getBudgetObject function to include missing comma in budget object

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,4 +1,6 @@
 export default function getBudgetObject(income, gdp, capita) {
-    const budget = {income, gdp, capita,};
+    const budget = {
+        income, gdp, capita,
+    };
     return budget;
   }
