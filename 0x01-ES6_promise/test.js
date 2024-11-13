265471a89ca4bd6aa0120f9734ac5ccf1c879344
youssef3332-a11y <youssef17102003@gmail.com>
2024-11-13 16:14:34 +0100

    Add getFullResponseFromAPI function and update test cases

--- 0x01-ES6_promise/test.js
+++ 0x01-ES6_promise/test.js
@@ -1,4 +1,3 @@
-import getResponseFromAPI from "./0-promise.js";
-
-const response = getResponseFromAPI();
-console.log(response instanceof Promise);
+import getFullResponseFromAPI from './1-promise';
+console.log(getFullResponseFromAPI(true));
+console.log(getFullResponseFromAPI(false));
